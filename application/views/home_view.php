085dd6626181673ac7dad6b253cfa3716893d198
Meinard Aeverie C. Quiambao <127920891+mnrdvrcqmb@users.noreply.github.com>
2023-04-17 08:19:20 +0800

    Test Comment

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -4,6 +4,6 @@
     <h2>Welcome to our karindirya</h2>
     <a class="btn btn-primary" href="<?= base_url('Home/view_products') ?>"  role="button">View Items</a>
     </div>
-
+<!-- This is a comment to let me know -->
 </div>
 
